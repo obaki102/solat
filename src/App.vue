251774dc0e6d2c74b3f2aae1d54477a30542ed98
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2024-02-27 07:34:27 +0800

    Added analytics.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { getAnalytics, logEvent } from "firebase/analytics";
+import { v4 as uuidv4 } from 'uuid';
 
+
+const analytics = getAnalytics();
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  let userId = localStorage.getItem('userId');
+
+  if (!userId) {
+    userId = generateUniqueId();
+    localStorage.setItem('userId', userId);
+  }
+
+  // Log a page_view event with the unique userId
+  logEvent(analytics,'page_view', {
+    userId,
+    page_location: window.location.href,
+    page_path: window.location.pathname,
+    page_title: document.title,
+  });
+});
+
+function generateUniqueId() {
+  return uuidv4(); 
+}
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,19 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import { initializeApp } from "firebase/app";
 import router from './router'
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+};
 
+initializeApp(firebaseConfig);
 const app = createApp(App)
 
 app.use(createPinia())
