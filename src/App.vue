e514b318d2256786acb47dee186dd79ce5306b70
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2024-03-18 14:31:35 +0800

    Disbale app in mobile for a moment while Im still working on the format for mobile.

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,29 @@
 import { RouterView } from 'vue-router'
 import { getAnalytics, logEvent } from "firebase/analytics"
 import { v4 as uuidv4 } from 'uuid';
-import { registerSW } from 'virtual:pwa-register'
 import { onMounted, ref } from 'vue';
 
+// import { registerSW } from 'virtual:pwa-register'
+// const updateSW = registerSW({
+//   onNeedRefresh() {
+//     // trigger a user prompt or just automatically
+//     updateSW()
+//   },
+//   onOfflineReady() {
+//     // show a ready to work offline to user
+//   },
+// })
+const isMobile = ref(false);
+const message = ref('');
+onMounted(() => {
+  if (/Mobi|Android/i.test(window.navigator.userAgent)) {
+    isMobile.value = true;
+    message.value = 'The mobile version is still under construction. Please use the web version.';
+  }
+});
 
-const updateSW = registerSW({
-  onNeedRefresh() {
-    // trigger a user prompt or just automatically
-    updateSW()
-  },
-  onOfflineReady() {
-    // show a ready to work offline to user
-  },
-})
 
 const analytics = getAnalytics();
-
 
 onMounted(() => {
   let userId = localStorage.getItem('userId');
@@ -28,7 +35,7 @@ onMounted(() => {
   }
 
   // Log a page_view event with the unique userId
-  logEvent(analytics,'page_view', {
+  logEvent(analytics, 'page_view', {
     userId,
     page_location: window.location.href,
     page_path: window.location.pathname,
@@ -37,15 +44,18 @@ onMounted(() => {
 });
 
 function generateUniqueId() {
-  return uuidv4(); 
+  return uuidv4();
 }
 </script>
 
 <template>
-  <div class="bg-white dark:bg-surat-500 min-h-screen pb-2">
+  <div v-if="isMobile" class="alert">
+    {{ message }}
+  </div>
+  <div v-else class="bg-white dark:bg-surat-500 min-h-screen pb-2">
     <RouterView />
   </div>
- 
+
 </template>
 
 <style scoped></style>
